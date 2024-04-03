f6d0e3583794484d0e9d4173c5a6de876dd99076
SzakacsAgi <szakacs.agi21@gmail.com>
2024-04-03 16:08:59 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const importAll = (r) => {
+    let images = {};
+    // eslint-disable-next-line array-callback-return
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  };
+  const images = importAll(require.context('./assets', false, /\.(png|jpe?g|svg)$/));
+  return <>
+    <section id="gallery">
+      <div className="gallery-container container">
+        <div className="tz-gallery">
+          <div className="row">
+            {Object.entries(images).map((image, index) => {
+              return <>
+                <div data-aos="flip-up" data-aos-delay={index*20} className="col-sm-6 col-md-4">
+                  <div className="thumbnail">
+                    <a className="lightbox" href={`${window.location.origin}${image[0]}`}>
+                      <img alt={image.toString()} src={`${window.location.origin}${image[1]}`}
+                           style={{width: "300px", height: "300px"}}/>
+                    </a>
+                  </div>
+                </div>
+              </>
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  </>
+
 }
 
 export default App;
